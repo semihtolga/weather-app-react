f661d7f3659d9478dbb6f4f42e50a563ca3590c1
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2022-11-04 13:24:27 +0300

    main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import { useState } from "react";
 
 const Main = () => {
-
+  const [searchText, setSearchText] = useState("ankara");
+  let apiKey = process.env.REACT_APP_API_KEY;
+  let units = "metric";
+  let lang = "tr";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${apiKey}&units=${units}&lang=${lang}`;
+  const iconUrl = "https://openweathermaporg/img/wn/icon@2x.png";
 
   return (
     <section className="main">
       <form>
-        <input type="text" placeholder='Seaach for a city' autoFocus />
-        <button type='submit'>SUBMIT</button>
-        <span className='msg'>error</span>
+        <input type="text" placeholder="Seaach for a city" autoFocus />
+        <button type="submit">SUBMIT</button>
+        <span className="msg">error</span>
       </form>
       <div>
-        <ul className='cities'></ul>
+        <ul className="cities"></ul>
       </div>
     </section>
-    
-  )
-}
+  );
+};
 
-export default Main 
+export default Main;
