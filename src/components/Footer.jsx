e85a9e13fc2ff68e4c376a447278e403e35e366a
Semih TOLGA <109283539+semihtolga@users.noreply.github.com>
2022-11-09 15:14:46 +0300

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="page-footer">
-      <div className="container">
-        <small>
-          Coded by <span>Semih TOLGA</span>
-        </small>
+    <footer className="footer-page d-flex align-items-center flex-column justify-content-center p-2 fs-6">
+      <div>
+        <a href="https://www.linkedin.com/in/semihtolga">
+          <i className="fab fa-linkedin me-3" />
+        </a>
+        <a href="https://github.com/semihtolga">
+          <i className="fa-brands fa-github me-3"></i>
+        </a>
       </div>
+      <article className="article" >Coded by Semih TOLGA</article>
     </footer>
   );
 };
